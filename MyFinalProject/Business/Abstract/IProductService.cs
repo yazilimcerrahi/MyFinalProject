a558d5340e3ea882eb12e3249cbbc39405cdcf1d
Yusuf Dağ <59394958+rosykick@users.noreply.github.com>
2021-02-10 22:28:31 +0300

    10 Şubat 1

--- MyFinalProject/Business/Abstract/IProductService.cs
+++ MyFinalProject/Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -13,5 +14,7 @@ namespace Business.Abstract
 
         List<Product> GetByUnitPrice(decimal min, decimal max);
         List<ProductDetailDto> GetProductDetails();
+        Product GetById(int productId);
+        IResult Add(Product product);
     }
 }
